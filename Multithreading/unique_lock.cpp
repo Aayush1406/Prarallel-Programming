985e2e095fd10a5601c61f22f1525891be6d5f82
Aayush Bhavsar <90923255+Aayush1406@users.noreply.github.com>
2025-07-06 20:04:19 -0800

    unique_lock.cpp

--- Multithreading/unique_lock.cpp
+++ Multithreading/unique_lock.cpp
@@ -44,3 +44,37 @@ int main(){
     return 0;
 
 }
+
+// example: 2
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int cnt = 0;
+mutex m;
+void f(){
+
+    for(int i=0;i<100;i++){
+    unique_lock<mutex> lock;
+        cnt++;
+    } // lock gets unlocked here !
+}
+
+int main(){
+
+    thread t1([](){f();});
+    thread t2([](){f();});
+    thread t3([](){f();});
+    thread t4([](){f();});
+    thread t5([](){f();});
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+
+    cout<<cnt;
+
+    return 0;
+}
