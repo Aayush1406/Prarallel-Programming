912504798df476ba80ed48a35e36739cd9426992
Aayush Bhavsar <90923255+Aayush1406@users.noreply.github.com>
2025-07-06 22:10:17 -0800

    cond_var.cpp

--- Multithreading/cond_var.cpp
+++ Multithreading/cond_var.cpp
@@ -35,3 +35,34 @@ int main(){
 
     return 0;
 }
+
+// example - Semaphore based sol
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+binary_semaphore add_sem{1};
+binary_semaphore with_sem{0};
+long bal = 0;
+
+void addMoney(int amt){
+
+    add_sem.acquire();
+    bal+=amt;
+    cout<<"Amt deposited = "<<amt<<" Total bal = "<<bal<<endl;
+    with_sem.release();
+}
+
+void withdrawMoney(int amt){
+
+    with_sem.acquire();
+    bal-=amt;
+    cout<<"Amt withdrawn = "<<amt<<" Total bal = "<<bal<<endl;
+    add_sem.release();
+}
+
+int main(){
+
+    return 0;
+}
