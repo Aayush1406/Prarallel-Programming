56b868818042ceb514d8ed10fed72443394dc59e
Aayush Bhavsar <90923255+Aayush1406@users.noreply.github.com>
2025-07-06 22:51:10 -0800

    Updated producer_consumer.cpp

--- Multithreading/producer_consumer.cpp
+++ Multithreading/producer_consumer.cpp
@@ -47,3 +47,76 @@ int main(){
 
     return 0;
 }
+
+// Bounded buffer with fixed cap in queue
+#include<bits/stdc++.h>
+using namespace std;
+
+class Test{
+    
+    private:
+        mutex m;
+        int cap;
+        queue<int> q;
+        int n;
+        condition_variable cv;
+
+    public:
+    Test(int cap = 5,int n = 30){
+        this->cap = cap;
+        this->n = n;
+    }
+
+    void producer(){
+
+        while(true){
+            
+            unique_lock<mutex> lock(m);
+            cv.wait(lock, [this](){if(q.size()<cap||(n==0))return true; else return false;});
+            if(n==0){
+                cv.notify_all();
+                break;
+            }
+            int num = rand();
+            q.push(num);
+            cout<<"Produced = "<<num<<endl;
+            this_thread::sleep_for(std::chrono::seconds(1));
+            n--;
+            cv.notify_all();
+        }
+    }
+
+    void consumer(){
+
+        while(true){
+            unique_lock<mutex> lock(m);
+            cv.wait(lock, [this](){if(q.size()!=0||n==0)return true; else return false;});
+            if(n==0){
+                cv.notify_all();
+                break;
+            }
+            int num = q.front();
+            cout<<"Consumed = "<<num<<endl;
+            this_thread::sleep_for(std::chrono::seconds(1));
+
+            q.pop();
+            n--;
+            cv.notify_all();
+
+        }
+
+    }
+};
+
+
+int main(){
+
+    Test o;
+    thread t1([&o](){o.producer();});
+    thread t2([&o](){o.consumer();});
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
